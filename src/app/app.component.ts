0680d4cfc3ffd875bfde3c10ee0ece6bb694ba98
Fernando_Abel <fernandosousa4k@gmail.com>
2025-03-13 21:29:14 -0300

    inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import {FooterComponent} from './footer/footer.component'
+import {HeaderComponent} from './header/header.component'
+import { CardComponent } from './card/card.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FooterComponent,HeaderComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
